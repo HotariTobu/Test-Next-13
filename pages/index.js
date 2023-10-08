32946c3825e465c1c52ab645e916c6387787d196
HotariTobu <hotari24tools@gmail.com>
2023-10-08 20:32:10 +0900

    Create-a-Next.js-App Editing-the-Page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -11,7 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
