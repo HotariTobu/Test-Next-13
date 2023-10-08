10548941e145eefc9b796ab8dd7dd81b6bb9dbe8
HotariTobu <hotari24tools@gmail.com>
2023-10-08 22:09:43 +0900

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -9,33 +9,37 @@ export default function FirstPost() {
   return (
     <Layout>
       {/* Navigate Between Pages */}
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </>
 
       {/* Assets, Metadata, and CSS */}
-      <div style={{ display: 'block' }}>
-        {/* Unoptimized image */}
-        <img src="/images/profile.jpg" alt="Your Name"></img>
+      <>
+        <div style={{ display: 'block' }}>
+          {/* Unoptimized image */}
+          <img src="/images/profile.jpg" alt="Your Name"></img>
 
-        {/* Optimized image */}
-        <Image src="/images/profile.jpg" height={144} width={144} alt="Your Name" />
-      </div>
+          {/* Optimized image */}
+          <Image src="/images/profile.jpg" height={144} width={144} alt="Your Name" />
+        </div>
 
-      <Head>
-        <title>First Post</title>
+        <Head>
+          <title>First Post</title>
 
-        {/* Warning */}
-        {/* <script src="https://connect.facebook.net/en_US/sdk.js"></script> */}
-      </Head>
+          {/* Warning */}
+          {/* <script src="https://connect.facebook.net/en_US/sdk.js"></script> */}
+        </Head>
 
-      {/* Outside Head component */}
-      <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload" onLoad={() => {
-        // needed `use client`?
-        console.log('script loaded correctly. window.FB has been populated')
-        console.log(FB)
-      }}></Script>
+        {/* Outside Head component */}
+        <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload" onLoad={() => {
+          // needed `use client`?
+          console.log('script loaded correctly. window.FB has been populated')
+          console.log(FB)
+        }}></Script>
+      </>
     </Layout>
   )
 }
