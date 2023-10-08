fbf6202f19c1d6f527a8083740bb45003af0946a
HotariTobu <hotari24tools@gmail.com>
2023-10-08 20:50:21 +0900

    Assets, Metadata, and CSS/Assets

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,23 @@
 import Link from "next/link"
+import Image from "next/image"
 
 export default function FirstPost() {
   return (
     <>
+      {/* Navigate Between Pages */}
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+
+      {/* Assets, Metadata, and CSS */}
+      <div style={{display: 'block'}}>
+        {/* Unoptimized image */}
+        <img src="/images/profile.jpg" alt="Your Name"></img>
+
+        {/* Optimized image */}
+        <Image src="/images/profile.jpg" height={144} width={144} alt="Your Name" />
+      </div>
     </>
   )
 }
