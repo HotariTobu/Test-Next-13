5f5388305714f1934cb050c1d2ab532663a43d42
HotariTobu <hotari24tools@gmail.com>
2023-10-09 12:38:57 +0900

    Dynamic Routes/Polishing the Post Page

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,15 +1,26 @@
+import Head from 'next/head'
+
 import Layout from '../../components/layout'
+import Date from '../../components/date'
+
+import utilStyles from '../../styles/utils.module.css'
+
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date}></Date>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
+      </article>
     </Layout>
   )
 }
